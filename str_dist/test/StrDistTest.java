890ae8243c3ce239e18c453cf36c9261673e5dc7
Tilmann Bruckhaus <Tilmann.Bruckhaus@progressfin.com>
2014-06-14 18:35:15 -0700

    add solution for recursive string match distance from CodingBat

---
+++ str_dist/test/StrDistTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class StrDistTest {
+    @Test
+    public void testStrDist() throws Exception {
+        assert 4 == StrDist.strDist("baba", "ba");
+        assert 2 == StrDist.strDist("ab", "ab");
+        assert 1 == StrDist.strDist("x", "x");
+        assert 0 == StrDist.strDist("", "");
+        assert 0 == StrDist.strDist("", "a");
+        assert 12 == StrDist.strDist("123456789012", "12");
+        assert 34 == StrDist.strDist("Some matching thing in the middle matches the sub-string", "match");
+        assert 46 == StrDist.strDist("Some matching thing in the middle matches the sub-string", "ing");
+        assert 6 == StrDist.strDist("ba, ba, black sheep, have you any wool?", "ba");
+        assert 42 == StrDist.strDist("ba, ba, black sheep, have you any wool? ba", "ba");
+        assert 0 == StrDist.strDist("ba, ba, black sheep, have you any wool? ba", "bam");
+        assert 28 == StrDist.strDist("abaababababbbbabaaababababbabbbbb", "a");
+    }
+}
