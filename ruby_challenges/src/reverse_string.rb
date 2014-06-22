ef6e899a3c09de3e4884bc0bde2be95a9e47cc63
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2014-06-22 10:13:42 -0700

    comment on avoiding String#reverse

--- ruby_challenges/src/reverse_string.rb
+++ ruby_challenges/src/reverse_string.rb
@@ -1,4 +1,5 @@
 def reverse_string(string)
+  # reverse string without using Ruby's String#reverse
   result = ''
   string.split('').each { |character| result = character + result}
   puts "Reverse of #{string} is #{result}"
