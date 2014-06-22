4138e42b0af5657d888de70b7788379717dce8a5
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2014-06-21 19:44:19 -0700

    add python solution for largest pandigital prime

---
+++ python_challenges/pandigital.py
@@ -0,0 +1,85 @@
+__author__ = 'tilmannbruckhaus'
+
+
+class Pandigital:
+    def __init__(self):
+        self.positions = 9 * [0]
+        self.max_length = 9
+        self.length = 1
+
+    def largest_prime(self):
+        """
+        All pandigital numbers of length 2, 3, 5, 6, 8, and 9 are non-prime.
+        The reason being that the sum of their digits is divisible by 3 and so these numbers are divisible by 3.
+        Hence, only pandigital numbers of length 4 or 7 can be prime.
+        """
+        largest_pandigital_prime = -1
+        for check_length in [4, 7]:
+            print "checking length:", check_length
+            self.length = check_length
+            candidate = self.current()
+            while self.length == check_length:
+                if candidate > largest_pandigital_prime:
+                    if is_prime(candidate):
+                        largest_pandigital_prime = candidate
+                candidate = self.next()
+        return largest_pandigital_prime
+
+    def current(self):
+        """
+        As we construct the pandigital number, we use an array of distinct digits.
+        We choose from the digits list position by position as indicated by the positions list.
+        As we construct the pandigital number we remove digits we used.
+        Example: if positions == [2, 0, 0] we generate 312.  For [1, 1, 0] we generate 231.
+        """
+        digits = range(1, 10)
+        result = 0
+        for i in range(0, self.length):
+            result *= 10
+            result += digits[self.positions[i]]
+            digits.pop(self.positions[i])
+        return result
+
+    def next(self):
+        """
+        Generate next pandigital number:
+        The positions list tracks in what order we choose from the digits list.
+        To generate the next legal positions list we increment digits in each position in turn.
+        We must check if incrementing the current position would exceed the legally available digits.
+        If incrementing is not legal we move to the next position.
+        Once all pandigital numbers of one length have been generated we increment the length of generated numbers.
+        After all pandigital numbers of legal length have been generated we return -1 to signal completion.
+        """
+        while self.length <= self.max_length:
+            for i in range(0, self.length - 1):
+                if i + self.positions[i] < self.length - 1:
+                    self.positions[i] += 1
+                    return self.current()
+                else:
+                    self.positions[i] = 0
+            if self.length == self.max_length:
+                return -1
+            self.length += 1
+            self.positions = 9 * [0]
+            return self.current()
+
+
+def is_prime(n):
+    """Check if integer n is a prime."""
+    # ensure n is a positive integer
+    n = abs(int(n))
+    # 0 and 1 are not primes
+    if n < 2:
+        return False
+    # 2 is the only even prime number
+    if n == 2:
+        return True
+    # all other even numbers are not primes
+    if not n & 1:
+        return False
+    # range starts with 3 and only needs to go up the square root of n
+    # for all odd numbers
+    for x in range(3, int(n ** 0.5) + 1, 2):
+        if n % x == 0:
+            return False
+    return True
---
+++ python_challenges/tests/test_pandigital.py
@@ -0,0 +1,9 @@
+__author__ = 'tilmannbruckhaus'
+
+import unittest
+import pandigital
+
+
+class TestPandigital(unittest.TestCase):
+    def test_pandigital(self):
+        self.assertEqual(7652413, pandigital.Pandigital().largest_prime())
