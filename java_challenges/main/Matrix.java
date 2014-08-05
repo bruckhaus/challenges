b87d5906e016dbcebb337553ad8f6db7fd61dc09
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2014-08-05 11:45:05 -0400

    refactor matrix traversal challenge to use a Matrix object

--- java_challenges/main/Matrix.java
+++ java_challenges/main/Matrix.java
@@ -1,32 +1,49 @@
 import java.util.Arrays;
 
 public class Matrix {
-    public static int traverse(int[][] a, int m, int n) {
-        int paths = 0;
 
-        System.out.println("\nMatrix.traverse:");
-        System.out.println("m: " + m);
-        System.out.println("n: " + n);
-        System.out.println("a:");
-        for (int i = 0; i < a.length; i++) {
-            System.out.println("    " + Arrays.toString(a[i]));
-        }
+    int[][] maze = new int[][]{{}};
+    int rows = 0;
+    int columns = 0;
 
-        paths = countPaths(a, m, n, 0, 0);
+    public Matrix(int[][] maze) {
+        this.maze = maze;
+        rows = maze.length;
+        columns = maze[0].length;
+    }
 
-        System.out.println("result: " + paths);
+    public int traverse() {
+        show();
+        int paths;
+        paths = countPaths(maze, 0, 0);
+        conclude(paths);
         return paths;
     }
 
-    private static int countPaths(int[][] a, int m, int n, int i, int j) {
-        if (i >= m || j >= n) {
+    private int countPaths(int[][] maze, int i, int j) {
+        if (i >= rows || j >= columns) {
             return 0;
-        } else if (a[i][j] == 0) {
+        } else if (maze[i][j] == 0) {
             return 0;
-        } else if (i == m - 1 && j == n - 1) {
+        } else if (i == rows - 1 && j == columns - 1) {
             return 1;
         } else {
-            return countPaths(a, m, n, i + 1, j) + countPaths(a, m, n, i, j + 1);
+            return countPaths(maze, i + 1, j) + countPaths(maze, i, j + 1);
         }
     }
+
+    private void show() {
+        System.out.println("\nMatrix.traverse:");
+        System.out.println("m: " + rows);
+        System.out.println("n: " + columns);
+        System.out.println("a:");
+        for (int[] aMaze : maze) {
+            System.out.println("    " + Arrays.toString(aMaze));
+        }
+    }
+
+    private void conclude(int paths) {
+        System.out.println("result: " + paths);
+    }
+
 }
--- java_challenges/test/MatrixTest.java
+++ java_challenges/test/MatrixTest.java
@@ -4,46 +4,44 @@ public class MatrixTest {
 
     @Test
     public void testTraverse() throws Exception {
-        assert 1 == Matrix.traverse(
-                new int[][]{
-                        {1}},
-                1, 1);
-        assert 1 == Matrix.traverse(
-                new int[][]{
-                        {1, 1},
-                        {0, 1}},
-                2, 2);
-        assert 2 == Matrix.traverse(
-                new int[][]{
-                        {1, 1, 1},
-                        {1, 0, 1},
-                        {1, 1, 1}},
-                3, 3);
-        assert 11 == Matrix.traverse(
-                new int[][]{
-                        {1, 1, 1, 1},
-                        {1, 1, 0, 1},
-                        {1, 1, 1, 1},
-                        {1, 1, 1, 1}},
-                4, 4);
-        assert 23 == Matrix.traverse(
-                new int[][]{
-                        {1, 1, 1, 1, 1},
-                        {1, 1, 1, 0, 1},
-                        {1, 1, 1, 1, 1},
-                        {1, 1, 1, 1, 1}},
-                4, 5);
-        assert 10 == Matrix.traverse(
-                new int[][]{
-                        {1, 1, 1, 1},
-                        {1, 1, 1, 1},
-                        {1, 1, 1, 1}},
-                3, 4);
-        assert 4 == Matrix.traverse(
-                new int[][]{
-                        {1, 1, 1, 1},
-                        {1, 1, 0, 1},
-                        {1, 1, 1, 1}},
-                3, 4);
+
+        assert 1 == new Matrix(new int[][]{{1}}).traverse();
+
+        assert 1 == new Matrix(new int[][]{
+                {1, 1},
+                {0, 1}
+        }).traverse();
+
+        assert 2 == new Matrix(new int[][]{
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        }).traverse();
+
+        assert 11 == new Matrix(new int[][]{
+                {1, 1, 1, 1},
+                {1, 1, 0, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1}
+        }).traverse();
+
+        assert 23 == new Matrix(new int[][]{
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 0, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+        }).traverse();
+
+        assert 10 == new Matrix(new int[][]{
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1}
+        }).traverse();
+
+        assert 4 == new Matrix(new int[][]{
+                {1, 1, 1, 1},
+                {1, 1, 0, 1},
+                {1, 1, 1, 1}
+        }).traverse();
     }
 }
