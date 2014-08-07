aa3b77dd13690d9ec440c2c301c110eec36c6823
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2014-08-07 01:01:29 -0400

    cleanup k-difference

--- java_challenges/main/KDifference.java
+++ java_challenges/main/KDifference.java
@@ -3,24 +3,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class KDifference {
+
     public static int kDifference(int n, int k, int[] a) {
+        showStart(n, k, a);
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i : a) {
+            if (map.containsKey(i - k)) count++;
+            if (map.containsKey(i + k)) count++;
+            map.put(i, 1);
+        }
+        showResult(count);
+        return count;
+    }
+
+    private static void showStart(int n, int k, int[] a) {
         System.out.println("\nkDifference:");
         System.out.println("n: " + n);
         System.out.println("k: " + k);
         System.out.println("a: " + Arrays.toString(a));
-
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int count = 0;
-        for (int i = 0; i < a.length; i++){
-            if (map.containsKey(a[i] - k)) {
-                count++;
-            }
-            if (map.containsKey(a[i] + k)) {
-                count++;
-            }
-            map.put(a[i], 1);
-        }
+    }
+    private static void showResult(int count) {
         System.out.println("result: " + count);
-        return count;
     }
 }
