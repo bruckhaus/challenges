0b876e6ec0e132f7748fee6c8ff521fee59cea0e
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2014-08-12 00:38:29 -0700

    limit precision to compare only accurate digits

--- java_challenges/test/ReversePolishTest.java
+++ java_challenges/test/ReversePolishTest.java
@@ -6,11 +6,11 @@ public class ReversePolishTest {
 
     @Test
     public void testEvaluate() {
-        Double eta = 1E-6d;
+        Double eta = 1E-5;
         assertEquals(2, ReversePolish.evaluate("1 1 +"), eta);
         assertEquals(-4, ReversePolish.evaluate("1 2 3 + -"), eta);
         assertEquals(0, ReversePolish.evaluate("1 2 + 3 -"), eta);
-        assertEquals(-2.666666, ReversePolish.evaluate("-4 2 * 3 /"), eta);
-        assertEquals(-38.6666679, ReversePolish.evaluate("-4 29 3 / *"), eta);
+        assertEquals(-2.66666, ReversePolish.evaluate("-4 2 * 3 /"), eta);
+        assertEquals(-38.66666, ReversePolish.evaluate("-4 29 3 / *"), eta);
     }
 }
