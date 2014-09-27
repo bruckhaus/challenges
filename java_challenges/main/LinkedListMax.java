64b6f18d72127fda956e0088b756499c29a96bb0
Tilmann Bruckhaus <Tilmann.Bruckhaus@gmail.com>
2014-09-27 02:21:10 -0700

    add Java solution for recursive implementation of maximum function for linked list

---
+++ java_challenges/main/LinkedListMax.java
@@ -0,0 +1,35 @@
+class Node {
+    int value;
+    Node next;
+
+    public Node(int v) {
+        value = v;
+        next = null;
+    }
+
+    public static int max(Node n) {
+        if (n == null) return Integer.MIN_VALUE;
+        return Math.max(n.value, max(n.next));
+    }
+}
+
+class List {
+
+    private Node first;
+    private Node last;
+
+    public List(int value) {
+        Node n = new Node(value);
+        first = n;
+        last = n;
+    }
+
+    public void append(int i) {
+        last.next = new Node(i);
+        last = last.next;
+    }
+
+    public int max() {
+        return Node.max(first);
+    }
+}
