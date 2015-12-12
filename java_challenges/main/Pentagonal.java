8a3f33dce184b6a0edcf65a32f458923000b23f5
Tilmann Bruckhaus <Tilmann_Bruckhaus@intuit.com>
2015-12-12 00:22:24 -0800

    wip project Euler problem 44

---
+++ java_challenges/main/Pentagonal.java
@@ -0,0 +1,16 @@
+public class Pentagonal {
+    public static long findNext() {
+        return 0;
+    }
+
+    public static boolean hasNext() {
+        return false;
+    }
+
+    public static long getNext() {
+        return 0;
+    }
+
+    public static void resetNext() {
+    }
+}
