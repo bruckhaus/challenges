dc2a604ec42329141baf29d86b7a4fa077be531c
Tilmann Bruckhaus <Tilmann.Bruckhaus@mail.mcgill.ca>
2016-01-01 10:55:36 -0800

    add ResourceFile class, refactor Prime1stMillion to use ResourceFile

---
+++ java_challenges/lib/src/main/java/resourceFile.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+class ResourceFile {
+
+    private final BufferedReader bufferedReader;
+
+    ResourceFile(String resourceFileName) throws FileNotFoundException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        @SuppressWarnings("ConstantConditions")
+        String fileName = classLoader.getResource(resourceFileName).getFile();
+        if (fileName == null)
+            throw new FileNotFoundException(resourceFileName + " not found");
+        File resourceFile = new File(fileName);
+        FileReader fileReader = new FileReader(resourceFile);
+        bufferedReader = new BufferedReader(fileReader);
+    }
+
+    public void close() throws IOException {
+        bufferedReader.close();
+    }
+
+    List getLines() throws IOException {
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) lines.add(line);
+        return lines;
+    }
+
+}
