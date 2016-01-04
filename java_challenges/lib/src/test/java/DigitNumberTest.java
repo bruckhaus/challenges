c2fae030144d714625fe8e6c3f3c5e21613427da
Tilmann Bruckhaus <Tilmann.Bruckhaus@mail.mcgill.ca>
2016-01-03 17:16:06 -0800

    add unit test for getSortedDigits()

--- java_challenges/lib/src/test/java/DigitNumberTest.java
+++ java_challenges/lib/src/test/java/DigitNumberTest.java
@@ -16,14 +16,23 @@ public class DigitNumberTest {
     @Test
     public void replaceDigit() throws Exception {
         ArrayList<int[]> replacements = new ArrayList<>();
-        replacements.add(new int[] {3, 0});
-        replacements.add(new int[] {4, 0});
-        replacements.add(new int[] {5, 0});
+        replacements.add(new int[]{3, 0});
+        replacements.add(new int[]{4, 0});
+        replacements.add(new int[]{5, 0});
         assertEquals(123000789L, DigitNumber.replaceDigits(123456789, replacements));
         replacements.clear();
-        replacements.add(new int[] {0, 9});
-        replacements.add(new int[] {1, 8});
-        replacements.add(new int[] {2, 7});
+        replacements.add(new int[]{0, 9});
+        replacements.add(new int[]{1, 8});
+        replacements.add(new int[]{2, 7});
         assertEquals(987456789L, DigitNumber.replaceDigits(123456789, replacements));
+    }
+
+    @Test
+    public void getSortedDigits() throws Exception {
+        char[] chars = new char[]{'0', '1', '2', '3', '4', '5', '6', '7'};
+        assertArrayEquals(chars, DigitNumber.getSortedDigits(76543210));
+        assertArrayEquals(chars, DigitNumber.getSortedDigits(32107654));
+        assertArrayEquals(chars, DigitNumber.getSortedDigits(12345670));
+        assertArrayEquals(chars, DigitNumber.getSortedDigits(10234567));
     }
 }
