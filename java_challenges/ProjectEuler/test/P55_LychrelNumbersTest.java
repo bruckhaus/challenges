6048de9f47df72252f026361f0117c9d144beea8
Tilmann Bruckhaus <Tilmann_Bruckhaus@intuit.com>
2016-01-21 15:12:36 -0800

    add some unit tests but need more

--- java_challenges/ProjectEuler/test/P55_LychrelNumbersTest.java
+++ java_challenges/ProjectEuler/test/P55_LychrelNumbersTest.java
@@ -20,11 +20,21 @@ public class P55_LychrelNumbersTest {
 
     @Test
     public void testGetLychrelTransform() throws Exception {
-
+        assertEquals(123321, P55_LychrelNumbers.getLychrelCount(123));
     }
 
     @Test
     public void testIsPalindrome() throws Exception {
-
+        assert (P55_LychrelNumbers.isPalindrome(1));
+        assert (P55_LychrelNumbers.isPalindrome(191));
+        assert (P55_LychrelNumbers.isPalindrome(123454321));
+        assert (P55_LychrelNumbers.isPalindrome(1001));
+        assert (P55_LychrelNumbers.isPalindrome(1234444321));
+        assert (!P55_LychrelNumbers.isPalindrome(12));
+        assert (!P55_LychrelNumbers.isPalindrome(123456789));
+        assert (!P55_LychrelNumbers.isPalindrome(1234564321));
+        assert (!P55_LychrelNumbers.isPalindrome(12398321));
+        assert (!P55_LychrelNumbers.isPalindrome(1231));
+        assert (!P55_LychrelNumbers.isPalindrome(1919129191));
     }
 }
