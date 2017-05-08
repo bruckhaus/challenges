6ba1be6138cc15c23246c27697789859e75b06a9
Tilmann Bruckhaus <Tilmann.Bruckhaus@mail.mcgill.ca>
2017-05-08 01:36:36 -0700

    refactor Project Euler problem 40, add unit tests

--- java_challenges/ProjectEuler/test/P40_ChampernownesConstantTest.java
+++ java_challenges/ProjectEuler/test/P40_ChampernownesConstantTest.java
@@ -5,36 +5,26 @@ import static org.junit.Assert.*;
 public class P40_ChampernownesConstantTest {
     @Test
     public void testFindSolution() throws Exception {
-        assertEquals(210, P40_ChampernownesConstant.findSolution());
+        assertEquals(1, P40_ChampernownesConstant.findSolution(0));
+        assertEquals(1, P40_ChampernownesConstant.findSolution(1));
+        assertEquals(5, P40_ChampernownesConstant.findSolution(2));
+        assertEquals(15, P40_ChampernownesConstant.findSolution(3));
+        assertEquals(105, P40_ChampernownesConstant.findSolution(4));
+        assertEquals(210, P40_ChampernownesConstant.findSolution(5));
+        assertEquals(210, P40_ChampernownesConstant.findSolution(6));
     }
 
     @Test
-    public void testInitialize() throws Exception {
-        P40_ChampernownesConstant.initialize();
+    public void testGetValue() throws Exception {
+        assertEquals(0, P40_ChampernownesConstant.getValue(1236543210, 0));
+        assertEquals(1, P40_ChampernownesConstant.getValue(1236543210, 1));
+        assertEquals(2, P40_ChampernownesConstant.getValue(1236543210, 2));
+        assertEquals(3, P40_ChampernownesConstant.getValue(1236543210, 3));
+        assertEquals(4, P40_ChampernownesConstant.getValue(1236543210, 4));
+        assertEquals(5, P40_ChampernownesConstant.getValue(1236543210, 5));
+        assertEquals(6, P40_ChampernownesConstant.getValue(1236543210, 6));
+        assertEquals(3, P40_ChampernownesConstant.getValue(1236543210, 7));
+        assertEquals(2, P40_ChampernownesConstant.getValue(1236543210, 8));
+        assertEquals(1, P40_ChampernownesConstant.getValue(1236543210, 9));
     }
-
-    @Test
-    public void testUpdateSolution() throws Exception {
-    }
-
-    @Test
-    public void testGetValueAtPosition() throws Exception {
-    }
-
-    @Test
-    public void testGetNextPosition() throws Exception {
-    }
-
-    @Test
-    public void testInitializePositions() throws Exception {
-    }
-
-    @Test
-    public void testPrintStringInfo() throws Exception {
-    }
-
-    @Test
-    public void testPrintValueInfo() throws Exception {
-    }
-
 }
